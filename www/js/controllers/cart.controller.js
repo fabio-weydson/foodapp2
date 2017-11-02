cfd6a9bb37a1654e7d74772bc601d826b29115ec
fabio-weydson <fabioweydson@gmail.com>
2017-11-01 23:08:56 -0300

    campo obs

--- www/js/controllers/cart.controller.js
+++ www/js/controllers/cart.controller.js
@@ -27,51 +27,54 @@ app
     if(window.AdMob) {
       //AdMob.showInterstitial();
     } 
-    $scope.update_obs = function(item) {
-      console.log(item)
+ 
+    // remove from cart
+    $scope.cartRemove = function(index) {
+       $scope.cartItems.splice(index, 1);
+       FCcart.setCartItems($scope.cartItems);
+    };
+
+    $scope.update_obs = function(item,key) {
+      item.PRA_Observacao = $scope.data.nova_obs;
       var cartItems = JSON.parse(localStorage.getItem('cart')) || [];
        angular.forEach(cartItems,function(i, v) {
               if (i && i.id == item.id) {    
                   cartItems.splice(v, 1, item);
                 }
         });
-       console.log(cartItems);
       localStorage.setItem('cart', JSON.stringify(cartItems));
     }
-    // remove from cart
-    $scope.cartRemove = function(index) {
-       $scope.cartItems.splice(index, 1);
-       FCcart.setCartItems($scope.cartItems);
-       console.log($scope.cartItems)
-    };
 
     $scope.showPopup = function(item,key) {
 
-   $scope.data = {}
+       $scope.data = {}
 
-   // An elaborate, custom popup
-   var myPopup = $ionicPopup.show({
-     template: '<input type="text" ng-change="update_obs(item)">',
-     title: 'Adicionar observação',
-     subTitle: 'Deseja retirar ou adicionar algo à este item?',
-     scope: $scope,
-     buttons: [
-       { text: 'Cancelar' },
-       {
-         text: '<b>Salvar</b>',
-         type: 'button-assertive',
-         onTap: function(e) {
-           if (item.PRA_Observacao) {
-             $scope.update_obs(item)
-             e.preventDefault();
-           } else {
-             return item.PRA_Observacao;
-           }
-         }
-       },
-     ]
-   });
-  };
+       // An elaborate, custom popup
+       var myPopup = $ionicPopup.show({
+           scope: $scope,
+         template: '<input type="text" ng-model="data.nova_obs"">',
+         title: 'Adicionar observação',
+         subTitle: 'Deseja retirar ou adicionar algo à este item?',
+      
+         buttons: [
+           { text: 'Cancelar' },
+           {
+             text: '<b>Salvar</b>',
+             type: 'button-assertive',
+             onTap: function(e) {
+               if (item.PRA_Observacao) {
+                 $scope.update_obs(item,key);
+                 myPopup.close();
+                 e.preventDefault();
+               } else {
+                 return item.PRA_Observacao;
+               }
+             }
+           },
+         ]
+       });
+
+    };
 
     $scope.$on('$ionicView.enter',function(){
 
